b87f4e5e07704b7d71d4d44c61d6aaf669eee2e5
Karissa <k@karissademi.com>
2014-08-15 15:16:42 -0400

    included included taxonomies and widgets in general

--- index.php
+++ index.php
@@ -1,2 +1,3 @@
 <?php 	// shut yo face
-require WPMU_PLUGIN_DIR.'/phila_general/phila_widgets.php';
+require WPMU_PLUGIN_DIR.'/phila_general/taxonomies.php';
+require WPMU_PLUGIN_DIR.'/phila_general/widgets.php';
---
+++ taxonomies.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Add custom taxonomies
+ *
+ * Additional custom taxonomies can be defined here
+ * http://codex.wordpress.org/Function_Reference/register_taxonomy
+ */
+function add_custom_taxonomies() {
+  // Add new "Locations" taxonomy to Posts
+  register_taxonomy('topics', 
+	array(
+  	'post',
+	'page',	
+	'phila_news'
+  ), array(
+    // Hierarchical taxonomy (like categories)
+    'hierarchical' => true,
+    // This array of options controls the labels displayed in the WordPress Admin UI
+    'labels' => array(
+      'name' => _x( 'Topics', 'taxonomy general name' ),
+      'singular_name' => _x( 'Topic', 'taxonomy singular name' ),
+      'search_items' =>  __( 'Search Topics' ),
+      'all_items' => __( 'All Topics' ),
+      //'parent_item' => __( 'Parent Topics' ),
+      //'parent_item_colon' => __( 'Parent Location:' ),
+      'edit_item' => __( 'Edit Topic' ),
+      'update_item' => __( 'Update Topic' ),
+      'add_new_item' => __( 'Add New Topic' ),
+      'new_item_name' => __( 'New Topic Name' ),
+      'menu_name' => __( 'Topics' ),
+    ),
+    // Control the slugs used for this taxonomy
+    'rewrite' => array(
+      'slug' => 'topics', // This controls the base slug that will display before each term
+      'with_front' => false, // Don't display the category base before "/locations/"
+      'hierarchical' => true // This will allow URL's like "/locations/boston/cambridge/"
+    ),
+  ));
+}
+add_action( 'init', 'add_custom_taxonomies', 0 );
