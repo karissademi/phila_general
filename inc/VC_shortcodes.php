d68247936a1c79cc97cf1ee8873a543debdbb4eb
Karissa <k@karissademi.com>
2014-09-15 11:55:57 -0400

    removal of "vc_text_separator" breaks entire site

--- inc/VC_shortcodes.php
+++ inc/VC_shortcodes.php
@@ -1,6 +1,11 @@
 <?php 
 /* 
-* Remove unneeded shortcodes from VC editor
+* Remove unneeded VC editor items
+	shortcodes
+	frontend editor
 */
 
-vc_remove_element("vc_text_separator");
+//vc_remove_element("vc_text_separator");
+
+
+//vc_disable_frontend();
